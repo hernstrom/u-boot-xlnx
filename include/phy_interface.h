fae3cd5cae1e634ae2b97923c34d06bc74fb0e6b
Ryan Campbell <ryan.campbell@ni.com>
2024-09-24 13:24:43 -0500

    Add moca as possible phy mode
    Upstream-Status: Pending
    
    %% original patch: 0001-Add-moca-as-possible-phy-mode.patch

--- include/phy_interface.h
+++ include/phy_interface.h
@@ -40,6 +40,7 @@ typedef enum {
 	PHY_INTERFACE_MODE_NCSI,
 	PHY_INTERFACE_MODE_10GBASER,
 	PHY_INTERFACE_MODE_USXGMII,
+	PHY_INTERFACE_MODE_MOCA,
 	PHY_INTERFACE_MODE_MAX,
 } phy_interface_t;
 
@@ -71,6 +72,7 @@ static const char * const phy_interface_strings[] = {
 	[PHY_INTERFACE_MODE_NCSI]		= "NC-SI",
 	[PHY_INTERFACE_MODE_10GBASER]		= "10gbase-r",
 	[PHY_INTERFACE_MODE_USXGMII]		= "usxgmii",
+	[PHY_INTERFACE_MODE_MOCA]		= "moca",
 };
 
 /* Backplane modes:
